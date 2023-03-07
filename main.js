4e41448a44d06fc22f8b4f719d6d8d087eb19574
Azad-mosarof <misarofazad@gmail.com>
2023-03-07 04:04:41 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
